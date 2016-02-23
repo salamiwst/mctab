a261b6b173e792eb79d7a12ff3becd9f06d9bf92
Thomas Lang <thomas.lang@student.tuwien.ac.at>
2016-02-23 15:59:11 +0100

    prohibit paragraph split

--- mctab.js
+++ mctab.js
@@ -4,10 +4,15 @@ $(function () {
     var clipboardText = e.originalEvent.clipboardData.getData("Text");
     if (clipboardText) {
       var longestLineWidth = $('#dummy').html(longestLine(clipboardText)).width();
+      $('#dummy').html('');
       console.log(longestLineWidth);
       $('#tab-content').css('-moz-column-width', longestLineWidth);
-      clipboardText = clipboardText.replace(/\n/g, "<br />");
-      $('#tab-content').html(clipboardText);
+      outputText = clipboardText;
+      outputText = '<div class="paragraph">\n' + outputText + '\n</div>';
+      outputText = outputText.replace(/^\s*[\r\n]/gm, '</div>\n<div class=\"paragraph\">\n');
+      outputText = outputText.replace(/\n/g, '<br />');
+      $('#tab-content').html(outputText);
+      //$('#tab-input').remove();
     } else {
       alert('Nothing to paste!');
     }
