291432d851e4484276a59ffbae1bd1375cb8fe4b
Thomas Lang <thomas.lang@student.tuwien.ac.at>
2016-02-23 17:13:37 +0100

    buttons for changing font size

--- mctab.js
+++ mctab.js
@@ -1,22 +1,42 @@
 $(function () {
+  var currentText = '';
+
+  $('#font-size-dec').button().click(function () {
+    changeFontSize(-1);
+    updateText();
+  });
+
+  $('#font-size-inc').button().click(function () {
+    changeFontSize(1);
+    updateText();
+  });
+
+  $('#change-text-size').buttonset();
+
   $('#tab-input').on('paste', function (e) {
     e.preventDefault();
     var clipboardText = e.originalEvent.clipboardData.getData("Text");
     if (clipboardText) {
-      var longestLineWidth = $('#dummy').html(longestLine(clipboardText)).width();
-      $('#dummy').html('');
-      console.log(longestLineWidth);
-      $('#tab-content').css('-moz-column-width', longestLineWidth);
-      outputText = clipboardText;
-      outputText = '<div class="paragraph">\n' + outputText + '\n</div>';
-      outputText = outputText.replace(/^\s*[\r\n]/gm, '</div>\n<div class=\"paragraph\">\n');
-      outputText = outputText.replace(/\n/g, '<br />');
-      $('#tab-content').html(outputText);
-      //$('#tab-input').remove();
+      currentText = clipboardText.trim();
+      updateText();
     } else {
-      alert('Nothing to paste!');
+      console.log('Nothing to paste!');
     }
   });
+
+  function updateText() {
+    $('#dummy').css('font-size', $('#tab-content').css('font-size'));
+    var longestLineWidth = $('#dummy').html(longestLine(currentText)).width();
+    $('#dummy').html('');
+    console.log(longestLineWidth);
+    $('#tab-content').css('-moz-column-width', longestLineWidth);
+    outputText = currentText;
+    outputText = '<div class="paragraph">\n' + outputText + '\n</div>';
+    outputText = outputText.replace(/^\s*[\r\n]/gm, '</div>\n<div class=\"paragraph\">\n');
+    outputText = outputText.replace(/\n/g, '<br />');
+    $('#tab-content').html(outputText);
+    //$('#tab-input').remove();
+  }
 
   function longestLine(text) {
     var i = 0;
@@ -38,6 +58,13 @@ $(function () {
       i++;
     }
     return longestLine;
+  }
+
+  function changeFontSize(d) {
+    var fontSize = parseInt($("#tab-content").css("font-size"));
+    fontSize = fontSize + d + "px";
+    $("#tab-content").css({'font-size': fontSize});
+    console.log(fontSize);
   }
 
 });
